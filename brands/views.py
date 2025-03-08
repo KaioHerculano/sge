457edee3e584bbcdde00440723398b2708168128
KaioHerculano <164403155+KaioHerculano@users.noreply.github.com>
2025-03-07 21:17:20 -0400

    Brand CRUD

--- brands/views.py
+++ brands/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from . import models, forms
 
 
@@ -7,6 +7,7 @@ class BrandListView(ListView):
     model = models.Brand
     template_name = 'brand_list.html'
     context_object_name = 'brands'
+    paginate_by = 10
 
     def get_queryset(self):
         queryset = super().get_queryset()
@@ -16,9 +17,28 @@ class BrandListView(ListView):
             queryset = queryset.filter(name__icontains=name)
 
         return queryset
-    
+
+
 class BrandCreateView(CreateView):
     model = models.Brand
     template_name = 'brand_create.html'
     form_class = forms.BrandForm
+    success_url = reverse_lazy('brand_list')
+
+
+class BrandDetailView(DetailView):
+    model = models.Brand
+    template_name = 'brand_detail.html'
+
+
+class BrandUpdateView(UpdateView):
+    model = models.Brand
+    template_name = 'brand_update.html'
+    form_class = forms.BrandForm
+    success_url = reverse_lazy('brand_list')
+
+
+class BrandDeleteView(DeleteView):
+    model = models.Brand
+    template_name = 'brand_delete.html'
     success_url = reverse_lazy('brand_list')
