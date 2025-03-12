3e4ebb4cb7adfb735150c1ad1cf615ba58db5661
KaioHerculano <164403155+KaioHerculano@users.noreply.github.com>
2025-03-12 00:56:58 -0400

    CRUD entries and admin created from all domains

--- inflows/admin.py
+++ inflows/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class InflowAdmin(admin.ModelAdmin):
+    list_display = ('product', 'quantity', 'created_at', 'updated_at')
+    search_fields = ('product__name', 'product__title',)
+
+
+admin.site.register(models.Inflow, InflowAdmin)
